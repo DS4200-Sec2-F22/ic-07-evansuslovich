ddaa6ad9e5cc8cd84c6edd864f0a93b605b58a9a
Evan Suslovich <evanjacobsuslovich@gmail.com>
2022-10-05 13:34:59 -0400

    clean up

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const data = [55000, 48000, 27000, 66000, 90000];
 const margins = { left: 50, right: 50, top: 50, bottom: 50 };
-const FRAME_HEIGHT = 200;
-const FRAME_WIDTH = 500;
+const FRAME_HEIGHT = 500;
+const FRAME_WIDTH = 200;
 
 const VIS_HEIGHT = FRAME_HEIGHT - margins.top - margins.bottom;
 const VIS_WIDTH = FRAME_WIDTH - margins.right - margins.left;
@@ -17,23 +17,23 @@ const FRAME3 = d3.select("#vis1")
 
 // Define scaling function 
 
-const MAX_X = d3.max(data, (d) => { return d; })
+const MAX_Y = d3.max(data, (d) => { return d; })
 
-const X_SCALE = d3.scaleLinear()
-  .domain([0, MAX_X])
-  .range([0, VIS_WIDTH])
+const Y_SCALE = d3.scaleLinear()
+  .domain([0, MAX_Y])
+  .range([0, VIS_HEIGHT])
 
 FRAME3.selectAll("points")
   .data(data)
   .enter()
   .append("circle")
-  .attr("cx", (d) => { return X_SCALE(d) + margins.left })
-  .attr("cy", margins.top)
-  .attr("r", 10)
+  .attr("cx", margins.left)
+  .attr("cy", (d) => { return Y_SCALE(d) + margins.top } )
+  .attr("r", 5)
   .attr("class", "point");
 
 FRAME3.append("g")
   .attr("transform",
-    "translate(" + margins.left + "," + (VIS_HEIGHT + margins.bottom) + ")")
-  .call(d3.axisBottom(X_SCALE).ticks(4))
+    "translate(" + (VIS_WIDTH + margins.top) + "," + (margins.top) + ")")
+  .call(d3.axisLeft(Y_SCALE).ticks(4))
   .attr("font-size", "20px")
